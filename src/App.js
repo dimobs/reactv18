11ac36699b06305fd3c38a59b2920719af983dc7
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-05-23 12:24:34 +0300

    ComponentDidMount

--- src/App.js
+++ src/App.js
@@ -4,16 +4,27 @@ import './App.css';
 function App() {
   const [name, setName] = useState('');
   const [count, setCount] = useState(0);
+  const [info, setInfo] = useState({
+    name: 'Dimo',
+    age: 41,
+    hobbies: ['first', 'secound', 'third']
+  });
 
+  //ComponentDidMount
   useEffect(() => {
     setTimeout(() => {
-      setName('Pesho')
+      setName('Pesho');
+      setInfo(oldState => ({
+        ...oldState,
+        age: 42,
+      }))
     }, 2000);
   }, []);
 
   return (
     <div className="App">
       <h2>Name: {!name ? 'Loading...' : name}</h2>
+      <h3>{info.age}  {info.name}   {info.hobbies[1]}</h3>
       <h3>{count}</h3>
       <button onClick={() => setCount(x => x + 1)}>increase</button>
     </div>
