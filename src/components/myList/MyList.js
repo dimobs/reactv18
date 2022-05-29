4faa795f4dab2b9ba8100c64644f0b7c326da7df
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-05-29 09:56:01 +0300

    Guards

--- src/components/myList/MyList.js
+++ src/components/myList/MyList.js
@@ -1,19 +1,26 @@
 import { useState, useEffect } from "react";
+import { useAuth } from '../../contexs/AuthContex';
+import { Navigate } from 'react-router-dom';
 
 const MyList = () => {
-const [dogs, setDogs] = useState([]);
+    const {user} = useAuth();
+    const [dogs, setDogs] = useState([]);
 
-useEffect(() =>{
-    fetch('https://dog.ceo/api/breeds/image/random/3')
-    .then(res => res.json())
-    .then(res => {
-        setDogs(res.message);
-    });
-}, []);
-console.log(dogs);
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random/3')
+            .then(res => res.json())
+            .then(res => {
+                setDogs(res.message);
+            });
+    }, []);
+
+    if (!user.email) {
+        return <Navigate to='/login' />
+    }
+
     return (
         <>
-        {dogs.map(x =>(<img className="d-block w-5" src={x} alt="Third slide" />))}
+            {dogs.map(x => (<img className="d-block w-5" src={x} alt="Third slide" />))}
         </>
     )
 }
