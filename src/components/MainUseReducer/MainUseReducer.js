ece75c14566147c6f9f81f9d5771caeb178a9e94
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-06-13 00:04:49 +0300

    Toggle and Delete Todo

--- src/components/MainUseReducer/MainUseReducer.js
+++ src/components/MainUseReducer/MainUseReducer.js
@@ -1,17 +1,27 @@
 import React, { useReducer, useState } from 'react';
 import Todo from './Todo';
 
-const ACTIONS = {
+export const ACTIONS = {
     ADD_TODO: 'add-todo',
-    TOGGLE_TODO: 'toggle-todo'
+    TOGGLE_TODO: 'toggle-todo',
+    DELETE_TODO: 'delete-todo'
 }
 
-function reducer(todos, action) {
-    switch (action.type) {
-        case ACTIONS.ADD_TODO:
-            return [...todos, newTodo(action.payload.name)]
-        case ACTIONS.TOGGLE_TODO:
-            return 'DAS';
+function reducer(todos, action) { //(todos-[], action - dispatcher(type, payload))
+      switch (action.type) {
+    case ACTIONS.ADD_TODO:
+        return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.TOGGLE_TODO:
+        return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+        return { ...todo, complete: !todo.complete }
+            }
+            return todo
+        })
+        case ACTIONS.DELETE_TODO:
+        return todos.filter(todo => todo.id !== action.payload.id)
+default: 
+return todos 
     }
 }
 
@@ -25,10 +35,13 @@ export default function MainUseReducer() {
 
     function handlerSubmit(e) {
         e.preventDefault();
-        dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name }})
+
+        dispatch({ 
+            type: ACTIONS.ADD_TODO, 
+            payload: { name: name } 
+        })
         setName('')
     }
-    console.log(todos);
 
     return (
         <>
@@ -36,7 +49,7 @@ export default function MainUseReducer() {
                 <input type="text" value={name} onChange={e => setName(e.target.value)} />
             </form>
             {todos.map(todo => {
-                return <Todo key={todo.id} todo={todo} />
+              return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
             })}
         </>
     )
