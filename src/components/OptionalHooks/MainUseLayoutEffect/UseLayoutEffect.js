3bffe9f4dd96a67c2418141ba3c0611b8b633591
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-06-16 20:17:31 +0300

    add funcionality

--- src/components/OptionalHooks/MainUseLayoutEffect/UseLayoutEffect.js
+++ src/components/OptionalHooks/MainUseLayoutEffect/UseLayoutEffect.js
@@ -1,12 +1,21 @@
-import React, {useState, useEffect, useLayoutEffect} from 'react'
+import React, {useState, useEffect, useLayoutEffect, useRef} from 'react'
 
 export default function UseLayoutEffect() {
-const [show, setShow] = useState(false)
+const [show, setShow] = useState(false);
+const popup = useRef();
+const button = useRef();
+useEffect(() => {
+    if(popup.current == null || button.current == null) return
+    const {button} = button.current.getBoudingClientReact()
+    popup.current.style.top = `${bottom + 25}px`
+},[show]);
 
   return (
     <div>
       <h2>Welcome use lay out effect</h2>
-    
+    <button ref={button} onClick={() => setShow(prev => !prev)}>
+        Click Here
+    </button>
     </div>
   )
 }
