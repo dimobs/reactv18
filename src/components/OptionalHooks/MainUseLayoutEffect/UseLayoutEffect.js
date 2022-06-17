fa99c2c4fe10eb728b9165c255805aad65a36cc4
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-06-17 18:37:34 +0300

    unVisible popUp message

--- src/components/OptionalHooks/MainUseLayoutEffect/UseLayoutEffect.js
+++ src/components/OptionalHooks/MainUseLayoutEffect/UseLayoutEffect.js
@@ -4,9 +4,9 @@ export default function UseLayoutEffect() {
 const [show, setShow] = useState(false);
 const popup = useRef();
 const button = useRef();
-useEffect(() => {
+useLayoutEffect(() => { //за разликата от useEffect, ще предтврати поп-ъп показването, което понякога е в идимо за потребителят
     if(popup.current == null || button.current == null) return
-    const {button} = button.current.getBoudingClientReact()
+    const {bottom} = button.current.getBoundingClientRect()
     popup.current.style.top = `${bottom + 25}px`
 },[show]);
 
@@ -16,6 +16,10 @@ useEffect(() => {
     <button ref={button} onClick={() => setShow(prev => !prev)}>
         Click Here
     </button>
+    {show && (
+        <div style={{position: 'absolute'}} ref={popup}>
+            This is a popup </div>
+    )}
     </div>
   )
 }
