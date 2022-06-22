ce2f26b1125d2e2fae6b91ed33c19f4386e0b45a
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2022-06-22 07:01:46 +0300

    test

--- src/components/CustomHooks/1-useToggle/test.js
+++ src/components/CustomHooks/1-useToggle/test.js
@@ -1,9 +1,39 @@
-function solve(input) {
-    const num = input.toString().split('');
-    const num1 = Number(num[0])
-    const num2 = Number(num[1])
-    const num3 = Number(num[2])
-    console.log('',num1,'\n',num2,'\n',num3);
+function substitute(input) {
+    let K = Number(input[0]); //(["6","7","5","6"]);
+    let L = Number(input[1]);
+    let M = Number(input[2]);
+    let N = Number(input[3]);
+ 
+    let combination = 0;
+    let isFInished = 6;
+ 
+    for (let i = K; i <= 8; i++){
+        for (let j = 9; j >= L; j--){
+            for (let k = M; k <= 8; k++){
+                for(let l = 9; l >= N; l--){
+                    if (i % 2 === 0 &&
+                        j % 2 === 1 && 
+                        k % 2 === 0 && 
+                        l % 2 === 1 && `${i}${j}` !== `${k}${l}`) {  
+                        console.log(`${i}${j} - ${k}${l}`);
+                        combination++;
+                        if (combination === 6) {
+                            isFInished = true;
+                            break;
+                        }
+                        } else if (i % 2 === 0 &&
+                                j % 2 === 1 && 
+                                k % 2 === 0 && 
+                                l % 2 === 1 && `${i}${j}` === `${k}${l}`) {
+                                console.log(`Cannot change the same player.`);
+                    }
+                }
+            } 
+        }
+        if (isFInished) {
+            break;
+        }   
+    } 
 }
-
-solve(105);
+ 
+substitute(103);
